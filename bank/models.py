cdd33097ff36a696d47e6c7308b4137202bd43c6
Anders Lassen <knh487@di.ku.dk>
2019-04-29 17:16:49 +0200

    CUS4-1

--- bank/models.py
+++ bank/models.py
@@ -77,3 +77,32 @@ def update_CheckingAccount(amount, CPR_number):
     cur.execute(sql, (amount, CPR_number))
     conn.commit()
     cur.close()
+    
+
+def select_investments(CPR_number):
+    cur = conn.cursor()
+    sql = """
+    SELECT i.account_number, a.cpr_number, a.created_date 
+    FROM investmentaccounts i
+    JOIN accounts a ON i.account_number = a.account_number    
+    WHERE a.cpr_number = %s
+    """
+    cur.execute(sql, (CPR_number,))
+    investment_accounts = Customers(cur.fetchall())
+    cur.close()
+    return investment_accounts
+
+def select_investments_with_certificates(CPR_number):
+    cur = conn.cursor()
+    sql = """
+    SELECT i.account_number, a.cpr_number, a.created_date
+    , cd.cd_number, start_date, maturity_date, rate, amount 
+    FROM investmentaccounts i
+    JOIN accounts a ON i.account_number = a.account_number
+    JOIN certificates_of_deposit cd ON i.account_number = cd.account_number    
+    WHERE a.cpr_number = %s
+    """
+    cur.execute(sql, (CPR_number,))
+    investment_certificates = Customers(cur.fetchall())
+    cur.close()
+    return investment_certificates
