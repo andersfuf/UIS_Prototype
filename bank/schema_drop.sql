77359cbf9362af5c16bad49253cf37122616e3c8
Anders Lassen <develop@lassena.dk>
2022-12-12 17:48:30 +0100

    fixing depedency ww_ removing view

--- bank/schema_drop.sql
+++ bank/schema_drop.sql
@@ -1,18 +1,19 @@
---DROP VIEW IF EXISTS vw_tdw;
---DROP VIEW IF EXISTS ww_invest_certificates;
---DROP VIEW IF EXISTS vw_invest_accounts;
+DROP VIEW IF EXISTS vw_tdw;
+DROP VIEW IF EXISTS vw_invest_accounts;
 DROP VIEW IF EXISTS vw_cd_sum;
+DROP VIEW IF EXISTS vw_invest_certificates;
 
 --
+
 DROP TABLE IF EXISTS Withdraws;
 DROP TABLE IF EXISTS Deposits;
+DROP TABLE IF EXISTS Certificates_of_deposit;
 -- ROLLBACK; commit;
 
+DROP TABLE IF EXISTS InvestmentAccounts;
 DROP TABLE IF EXISTS CheckingAccounts;
-DROP TABLE IF EXISTS Certificates_of_deposit;
 --
 DROP TABLE IF EXISTS Transfers;
-DROP TABLE IF EXISTS InvestmentAccounts;
 DROP TABLE IF EXISTS Manages;
 --
 DROP TABLE IF EXISTS Accounts;
